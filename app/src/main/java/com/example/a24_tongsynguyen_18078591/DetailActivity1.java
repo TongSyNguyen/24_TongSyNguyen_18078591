ff300505548236bffbbd49e4e3cfcf7af5b57829
DELL <tongsynguyen22@gmail.com>
2021-11-26 17:11:08 +0700

    Hoàn thành bài thiếu phần service và login with checkbox

--- app/src/main/java/com/example/a24_tongsynguyen_18078591/DetailActivity1.java
+++ app/src/main/java/com/example/a24_tongsynguyen_18078591/DetailActivity1.java
@@ -2,16 +2,26 @@ package com.example.a24_tongsynguyen_18078591;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class DetailActivity1 extends AppCompatActivity {
-
+    ImageView tinhtien;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail1);
+
+        tinhtien = findViewById(R.id.tinhtien);
+        tinhtien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailActivity1.this, DetailActivity2.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/example/a24_tongsynguyen_18078591/DetailActivity2.java
@@ -0,0 +1,14 @@
+package com.example.a24_tongsynguyen_18078591;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail2);
+    }
+}
